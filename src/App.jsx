3e3733d9f585e985d7c8ce7de0f52d15d241e7f7
Deyvid Matheus <deyvid.nascimento0101@gmail.com>
2024-07-24 06:35:55 -0300

    Project started

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import '../src/styles/AnimatedBackground.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="context">
+        <h1>Deyvid Matheus</h1>
+        <br />
+        <br />
+        <h2 className="contextSecondH1">Trilha de Aprendizagem em S.I</h2>
+      </div>
+      
+      <div className="area" >
+                <ul className="circles">
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                </ul>
+        </div >
+    </>
+  )
+}
+
+export default App
