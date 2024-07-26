fabcc1aeb54a6bd831a3024be66b274ef60cc157
Deyvid Matheus <deyvid.nascimento0101@gmail.com>
2024-07-25 21:46:37 -0300

    Project update

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,24 @@
-import '../src/styles/AnimatedBackground.css'
+import '../src/styles/styles.css'
+import '../src/styles/global.css'
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 function App() {
 
   return (
     <>
       <div className="context">
+        <img className="avatar" src="https://github.com/dwyvid1.png" />
         <h1>Deyvid Matheus</h1>
-        <br />
-        <br />
-        <h2 className="contextSecondH1">Trilha de Aprendizagem em S.I</h2>
+        <h2>Trilha de Aprendizagem em S.I</h2>
+
+        <div className='links'>
+          <a href="https://github.com/dwyvid1" target="blank"><GitHubIcon/> Github</a>
+          <a href="https://www.linkedin.com/in/deyvid-matheus-101a69197/" target="blank"><LinkedInIcon/> LinkedIn</a>
+          <a href="#"><InstagramIcon/>Instagram</a>
+        </div>
       </div>
-      
-      <div className="area" >
-                <ul className="circles">
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                </ul>
-        </div >
     </>
   )
 }
