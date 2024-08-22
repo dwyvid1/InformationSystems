c7fe896ea3946f3b9edeb98d113a1393fc89c1cb
Deyvid Matheus <deyvid.nascimento0101@gmail.com>
2024-08-21 23:46:56 -0300

    Updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
 import '../src/styles/styles.css'
 import '../src/styles/global.css'
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import InstagramIcon from '@mui/icons-material/Instagram';
+
+import About from './components/About';
 
 function App() {
 
   return (
     <>
       <div className="context">
-        <img className="avatar" src="https://github.com/dwyvid1.png" />
-        <h1>Deyvid Matheus</h1>
-        <h2>Trilha de Aprendizagem em S.I</h2>
+        <h1>Manual do Aluno de <br />
+        Sistemas de Informação</h1>
 
-        <div className='links'>
-          <a href="https://github.com/dwyvid1" target="blank"><GitHubIcon/> Github</a>
-          <a href="https://www.linkedin.com/in/deyvid-matheus-101a69197/" target="blank"><LinkedInIcon/> LinkedIn</a>
-          <a href="#"><InstagramIcon/>Instagram</a>
-        </div>
+        <About />
       </div>
     </>
   )
---
+++ src/components/About.jsx
@@ -0,0 +1,25 @@
+import '../styles/global.css'
+import '../styles/styles.css'
+import '../styles/About.css'
+
+function About() {
+    return (
+        <>  
+            <div className="about-container">
+               <h3>Sobre o Projeto</h3>
+                <h4>Bem-vindo ao Manual do Aluno de Sistemas de Informação!</h4> 
+                <p>
+                    Este projeto surgiu da necessidade de organizar e compartilhar o conhecimento adquirido ao longo do curso de Sistemas de Informação. A ideia é criar um repositório digital abrangente e acessível, que reúna os principais conceitos, práticas e recursos que um estudante da área deve conhecer.
+                    A motivação para criar este site é duplamente prática e pessoal. Em primeiro lugar, ao documentar e estruturar o material que estou estudando, posso revisitar e consolidar o conhecimento de forma mais eficiente. Em segundo lugar, desejo contribuir para a comunidade acadêmica e para futuros alunos que podem se beneficiar de uma fonte de informação clara e organizada sobre tópicos essenciais da área de Sistemas de Informação.
+                </p>
+
+                <p>
+                    O Manual do Aluno de Sistemas de Informação abrange uma ampla gama de temas, desde fundamentos teóricos até práticas de mercado. Esperamos que este site seja uma ferramenta valiosa não apenas para quem está começando na área, mas também para aqueles que buscam aprofundar seus conhecimentos e acompanhar as tendências e melhores práticas.
+                    Convido você a explorar o conteúdo, aproveitar os recursos disponíveis e, se desejar, contribuir com sugestões e feedbacks para que possamos melhorar continuamente o Manual. Juntos, podemos construir um espaço de aprendizado dinâmico e colaborativo!
+                </p>
+            </div>
+        </>
+    )
+}
+
+export default About;
