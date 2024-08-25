bc591cdef4e09813e7d917cc4536215742ccc56b
Deyvid Matheus <deyvid.nascimento0101@gmail.com>
2024-08-24 21:58:15 -0300

    Additional information

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,13 +5,21 @@ function About() {
     return (
         <>  
             <div className="about-container">
-                <h1>Bem-vindo ao Manual do Aluno de Sistemas de Informação!</h1> 
+                <h1>Bem-vindo ao Manual do Aluno de Sistemas de Informação</h1> 
                 <p>
                     Este projeto foi desenvolvido para organizar e compartilhar o conhecimento adquirido no curso de Sistemas de Informação. O objetivo é criar um repositório digital que compile conceitos, práticas e recursos essenciais para estudantes da área, facilitando o acesso e a revisão do material. A motivação para a criação do site é dupla: consolidar o próprio conhecimento de forma eficiente e contribuir com uma fonte de informação clara e organizada para a comunidade acadêmica e futuros alunos.
                 </p>
-
                 <p>
                     O Manual do Aluno de Sistemas de Informação aborda uma ampla gama de temas, desde fundamentos teóricos até práticas de mercado, com o intuito de ser uma ferramenta valiosa tanto para iniciantes quanto para aqueles que desejam aprofundar seus conhecimentos e acompanhar as tendências e melhores práticas da área. O site convida os usuários a explorar o conteúdo, aproveitar os recursos disponíveis e oferecer sugestões e feedback para ajudar na melhoria contínua do Manual.
+                </p>
+
+                <h1>Sobre o curso de</h1>
+                <h2>Sistemas de Informação</h2>
+
+                <img src="https://i.pinimg.com/564x/60/55/ae/6055aea749f9b6a59aae4595c5d3175c.jpg" alt="Imagem" />
+
+                <p>
+                O curso de Sistemas de Informação é voltado para a formação de profissionais capazes de projetar, implementar e gerenciar soluções tecnológicas para atender às necessidades das organizações. O currículo geralmente abrange áreas como análise de sistemas, desenvolvimento de software, banco de dados, redes de computadores e segurança da informação. Além disso, o curso também pode incluir disciplinas de gestão de projetos e de negócios para garantir que os alunos estejam preparados para trabalhar tanto com tecnologia quanto com aspectos administrativos e estratégicos das empresas. A formação busca equilibrar o conhecimento técnico com habilidades práticas e teóricas para promover uma visão abrangente da área.
                 </p>
             </div>
         </>
